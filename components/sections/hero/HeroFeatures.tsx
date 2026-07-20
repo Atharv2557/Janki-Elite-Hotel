c49e75eda210ce05933817284cc083303e3100ca
Atharv2557 <atharvmangal245@gmail.com>
2026-07-20 16:46:01 +0530

    change in home features in hero section

--- components/sections/hero/HeroFeatures.tsx
+++ components/sections/hero/HeroFeatures.tsx
@@ -10,26 +10,31 @@ import {
 
 export default function HeroFeatures() {
   return (
-    <div className="absolute bottom-0 left-0 z-30 hidden w-full px-8 pb-5 lg:block">
+    <div className="absolute bottom-0 left-0 z-30 w-full px-4 pb-4 sm:px-6 sm:pb-5 lg:px-8">
       <motion.div
         variants={heroFeaturesContainer}
         initial="hidden"
         animate="visible"
         className="
           mx-auto
-          flex
+          grid
           max-w-7xl
-          items-center
-          justify-between
-          gap-4
-          rounded-full
+          grid-cols-2
+          gap-2
+          rounded-[26px]
           border
           border-white/15
           bg-white/10
-          px-8
-          py-4
+          p-3
           text-center
           backdrop-blur-xl
+          sm:gap-3
+          sm:rounded-[30px]
+          sm:p-4
+          lg:grid-cols-4
+          lg:rounded-full
+          lg:px-6
+          lg:py-4
         "
       >
         {heroContent.features.map((feature) => (
@@ -41,13 +46,19 @@ export default function HeroFeatures() {
               border
               border-white/10
               bg-black/10
-              px-4
+              px-3
               py-2
-              text-[11px]
-              font-medium
+              text-[9px]
+              font-semibold
               uppercase
-              tracking-[0.22em]
+              leading-none
+              tracking-[0.16em]
               text-white/85
+              sm:px-4
+              sm:text-[10px]
+              sm:tracking-[0.2em]
+              lg:text-[11px]
+              lg:tracking-[0.22em]
             "
           >
             {feature}
